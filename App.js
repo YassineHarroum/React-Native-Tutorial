ecb7e948910d24858414ad83575023a98f5d24c3
yassine harroum <yassine.harroume@gmail.com>
2022-03-20 16:45:48 +0100

    Course react native pratice -TouchableOpacity Highlight Pressable and its props- section 12

--- App.js
+++ App.js
@@ -15,29 +15,51 @@ import {
   TextInput,
   SectionList,
   RefreshControl,
+  Pressable,
 } from 'react-native';
 
 
 const App = () => {
-  const [name,setname] = useState('');
+  const [name, setname] = useState('');
+  const [submitted, setsubmitted] = useState(false);
+  const onPressHandler = () => {
+    setsubmitted(!submitted);
+  };
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Enter your email :</Text>
+      <Text style={styles.text}>User email :</Text>
       <TextInput
         style={styles.textinput}
-        onChangeText={(value)=>{setname(value)}}
+        onChangeText={(value) => { setname(value) }}
         placeholder='user email'
       />
-      <Text style={styles.text}>Enter your password :</Text>
+      <Text style={styles.text}>Password :</Text>
       <TextInput
         style={styles.textinput}
-        onChangeText={(value)=>{setname(value)}}
         placeholder='password'
         keyboardType='numeric'
         secureTextEntry
       />
-      <Text style={styles.text}>{name}</Text>
+      <Pressable
+        onPress={onPressHandler}
+        hitSlop={{ top: 25, bottom: 25, right: 25, left: 25 }}
+        style={() => [
+          { backgroundColor: submitted ? '#b101f1' : '#01bff1' },
+          { borderColor: submitted ? '#b101f1' : '#01bff1' },
+
+          styles.button
+        ]}
+      >
+        <Text style={styles.text}>
+          {submitted ? 'clear' : 'Sign up'}
+        </Text>
+      </Pressable>
+      {submitted ?
+        <Text style={styles.text}>you are sign as : {name}</Text>
+        :
+        null
+      }
     </View>
   );
 };
@@ -58,10 +80,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   textinput: {
-    width : 150,
-    borderWidth : 1,
-    textAlign :'center',
-    fontSize : 18,
+    width: 250,
+    borderWidth: 1,
+    borderRadius: 20,
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  button: {
+    // backgroundColor: '#01bff1',
+    width: 100,
+    margin: 20,
+    borderWidth: 1,
+    // borderColor: '#01bff1',
+    borderRadius: 15,
+
   }
 });
 
